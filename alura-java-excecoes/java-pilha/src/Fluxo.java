98d918bfbddbc7f7c4d0c916e41a2a3f303362e2
RodrigoD324 <rodrigo.silvadeoliveira324@gmail.com>
2022-12-03 13:35:00 -0300

    atualizando a classe 'Fluxo'

--- alura-java-excecoes/java-pilha/src/Fluxo.java
+++ alura-java-excecoes/java-pilha/src/Fluxo.java
@@ -12,13 +12,13 @@ public class Fluxo {
 		System.out.println("Fim do main");
 	}
 
-	private static void metodo1() {
+	private static void metodo1() throws MinhaException {
 		System.out.println("Ini do metodo1");
 		metodo2();
 		System.out.println("Fim do metodo1");
 	}
 
-	private static void metodo2() {
+	private static void metodo2() throws MinhaException  {
 		System.out.println("Ini do metodo2");
 		
 		throw new MinhaException("deu muito errado!"); 
