c9d017a1f8e6901718d303a7402eeb066c53159d
RodrigoD324 <rodrigo.silvadeoliveira324@gmail.com>
2022-12-03 15:54:39 -0300

    modifiquei as classes 'Conexao' e 'TesteConexao'

--- alura-java-excecoes/java-pilha/src/Conexao.java
+++ alura-java-excecoes/java-pilha/src/Conexao.java
@@ -1,15 +1,17 @@
-public class Conexao {
+public class Conexao implements AutoCloseable {
 
 	public Conexao() {
 		System.out.println("Abrindo conexao");
+		throw new IllegalStateException();
 	}
 	
 	public void leDados() {
 		System.out.println("Recebendo dados");
 		throw new IllegalStateException();
 	}
-	
-	public void fecha() {
+
+	@Override
+	public void close() {
 		System.out.println("Fechando conexao");
 	}
 	
--- alura-java-excecoes/java-pilha/src/TesteConexao.java
+++ alura-java-excecoes/java-pilha/src/TesteConexao.java
@@ -2,15 +2,26 @@ public class TesteConexao {
 
 	public static void main(String[] args) {
 
-		Conexao con = null;
-		try {
-			con = new Conexao();
-			con.leDados();
+		try (Conexao conexao = new Conexao()) {
+			conexao.leDados();
 		} catch (IllegalStateException ex) {
 			System.out.println("Deu erro na conexao");
-		} finally {
-			con.fecha();
 		}
+
+		// -----------------------------------------------------------------------------------
+
+//		Conexao con = null;
+//		try {
+//			con = new Conexao();
+//			con.leDados();
+//		} catch (IllegalStateException ex) {
+//			System.out.println("Deu erro na conexao");
+//		} finally {
+//			System.out.println("finally");
+//			if (con != null) {
+//				con.close();
+//			}
+//		}
 
 	}
 
