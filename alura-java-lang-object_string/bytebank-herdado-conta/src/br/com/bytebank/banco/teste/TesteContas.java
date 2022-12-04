20b84b259964e61da7c8727c9ca305336347d05b
RodrigoD324 <rodrigo.silvadeoliveira324@gmail>
2022-12-04 11:50:44 -0300

    adicionei o package 'especial'

--- alura-java-lang-object_string/bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteContas.java
+++ alura-java-lang-object_string/bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteContas.java
@@ -1,13 +1,13 @@
 package br.com.bytebank.banco.teste;
 
 import br.com.bytebank.banco.modelo.SaldoInsuficienteException;
+import br.com.bytebank.banco.especial.ContaEspecial;
 import br.com.bytebank.banco.modelo.ContaCorrente;
 import br.com.bytebank.banco.modelo.ContaPoupanca;
 
 public class TesteContas {
 
 	public static void main(String[] args) throws SaldoInsuficienteException {
-		//Full Qualified Name - FQN
 		ContaCorrente cc = new ContaCorrente(111, 111);
 		cc.deposita(100.0);
 		
