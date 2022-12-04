bfef290ac1a74b4bf20da9bea8c9bfc6b2238b0f
RodrigoD324 <rodrigo.silvadeoliveira324@gmail>
2022-12-04 19:09:35 -0300

    removi o diretório 'doc'

--- alura-java-lang-object_string/bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteString.java
+++ alura-java-lang-object_string/bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteString.java
@@ -6,38 +6,38 @@ public class TesteString {
 
 		String vazio = "         Alura         ";
 		String outroVazio = vazio.trim();
-		
+
 		System.out.println(vazio.contains("Alu"));
 		System.out.println(outroVazio);
 		System.out.println(outroVazio.isEmpty());
-		
+
 		String nome = "Alura"; // object literal
-		//String outro = new String("Alura");
-		
+		// String outro = new String("Alura");
+
 		System.out.println(nome.length());
-		
-		for(int i = 0; i < nome.length(); i++) {
+
+		for (int i = 0; i < nome.length(); i++) {
 			System.out.println(nome.charAt(i));
 		}
-		
-//		String sub = nome.substring(1);
-//		System.out.println(sub);
-		
-//		int pos = nome.indexOf("ur");
-//		System.out.println(pos);
-		
-//		char c = nome.charAt(2);
-//		System.out.println(c);
-		
+
+		String sub = nome.substring(1);
+		System.out.println(sub);
+
+		int pos = nome.indexOf("ur");
+		System.out.println(pos);
+
+		char c = nome.charAt(2);
+		System.out.println(c);
+
 //		char c = 'A';
-//		char d = 'a';	
-//		String outro = nome.replace(c , d);
-		
+//		char d = 'a';
+//		String outro = nome.replace(c, d);
+
 		//String outro = nome.toUpperCase();
-		
+
 //		System.out.println(nome);
 //		System.out.println(outro);
-		
+
 	}
 
 }
