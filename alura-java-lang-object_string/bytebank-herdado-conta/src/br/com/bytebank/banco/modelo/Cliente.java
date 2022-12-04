045732827f9660c8089448d9e09f8118f2bec443
RodrigoD324 <rodrigo.silvadeoliveira324@gmail>
2022-12-04 13:59:51 -0300

    gerei o javadoc para o projeto

--- alura-java-lang-object_string/bytebank-herdado-conta/src/br/com/bytebank/banco/modelo/Cliente.java
+++ alura-java-lang-object_string/bytebank-herdado-conta/src/br/com/bytebank/banco/modelo/Cliente.java
@@ -1,5 +1,12 @@
 package br.com.bytebank.banco.modelo;
 
+/**
+ * Classe que representa um cliente no Bytebank.
+ * 
+ * @author Rodrigo
+ * @version 0.1
+ */
+
 public class Cliente {
 	
 	private String nome;
--- alura-java-lang-object_string/bytebank-herdado-conta/src/br/com/bytebank/banco/modelo/Conta.java
+++ alura-java-lang-object_string/bytebank-herdado-conta/src/br/com/bytebank/banco/modelo/Conta.java
@@ -1,5 +1,12 @@
 package br.com.bytebank.banco.modelo;
 
+/**
+ * Classe representa a moldura de uma conta.
+ * 
+ * @author Rodrigo
+ * @version 0.1
+ */
+
 public abstract class Conta {
 
 	protected double saldo;
@@ -7,7 +14,13 @@ public abstract class Conta {
 	private int numero;
 	private Cliente titular;
 	private static int total = 0;
-
+	
+	/**
+	 * Construtor para inicializar o objeto Conta a partir da agencia e numero.
+	 * 
+	 * @param agencia
+	 * @param numero
+	 */
 	public Conta(int agencia, int numero) {
 		Conta.total++;
 		// System.out.println("O total de contas e: " + Conta.total);
@@ -18,7 +31,13 @@ public abstract class Conta {
 	}
 
 	public abstract void deposita(double valor);
-
+	
+	/**
+	 * Valor precisa ser maior do que o saldo.
+	 * 
+	 * @param valor
+	 * @throws SaldoInsuficienteException
+	 */
 	public void saca(double valor) throws SaldoInsuficienteException {
 
 		if (this.saldo < valor) {
