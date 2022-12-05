7f1c1ea849402c03d4abac469c9bdb7869ba4521
RodrigoD324 <rodrigo.silvadeoliveira324@gmail.com>
2022-12-05 18:41:00 -0300

    atualizado a classe 'TesteArrayReferencias'

--- alura-java-util-arraysWrappersColecoesLambda/bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteArrayReferencias.java
+++ alura-java-util-arraysWrappersColecoesLambda/bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteArrayReferencias.java
@@ -1,25 +1,29 @@
 package br.com.bytebank.banco.teste;
 
+import br.com.bytebank.banco.modelo.Cliente;
+import br.com.bytebank.banco.modelo.Conta;
 import br.com.bytebank.banco.modelo.ContaCorrente;
+import br.com.bytebank.banco.modelo.ContaPoupanca;
 
 public class TesteArrayReferencias {
 
 	public static void main(String[] args) {
 
 		//int[] idades = new int[5];
-		ContaCorrente[] contas = new ContaCorrente[5];
+		Object[] referencias = new Object[5];
+		
 		ContaCorrente cc1 = new ContaCorrente(22, 11);
-		contas[0] = cc1;
+		referencias[0] = cc1;
 		
-		ContaCorrente cc2 = new ContaCorrente(22, 22);
-		contas[1] = cc2;
+		ContaPoupanca cc2 = new ContaPoupanca(22, 22);
+		referencias[1] = cc2;
 		
-		///System.out.println(cc2.getNumero());
-		System.out.println(contas[1].getNumero());
+		Cliente cliente = new Cliente();
+		referencias[2] = cliente;
 		
-		ContaCorrente ref = contas[1];
+		//Conta ref = referencias[1];
 		System.out.println(cc2.getNumero());
-		System.out.println(ref.getNumero());
+		//System.out.println(ref.getNumero());
 		
 	}
 
