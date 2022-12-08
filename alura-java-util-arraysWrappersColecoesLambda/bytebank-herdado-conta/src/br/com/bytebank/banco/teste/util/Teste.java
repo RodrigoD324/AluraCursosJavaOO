1e22a7e1a02882a40d896c6583c34ee583569ca3
RodrigoD324 <rodrigo.silvadeoliveira324@gmail.com>
2022-12-08 13:17:33 -0300

    descobrindo o que é autoboxing em List

---
+++ alura-java-util-arraysWrappersColecoesLambda/bytebank-herdado-conta/src/br/com/bytebank/banco/teste/util/Teste.java
@@ -0,0 +1,17 @@
+package br.com.bytebank.banco.teste.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Teste {
+
+	public static void main(String[] args) {
+
+		int[] idade = new int[5];
+		String[] nomes = new String[5];
+		
+		List numeros = new ArrayList<>();
+		
+	}
+
+}
