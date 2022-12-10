a7af85fc8e15b003cb1ade05bebc68ce35f9ee22
RodrigoD324 <rodrigo.silvadeoliveira324@gmail.com>
2022-12-10 14:51:38 -0300

    treinando escrita com com java.io

--- alura-java-io-StreamsReaderWriters/java-io/src/br/com/alura/java/io/teste/TesteLeitura.java
+++ alura-java-io-StreamsReaderWriters/java-io/src/br/com/alura/java/io/teste/TesteLeitura.java
@@ -1,25 +1,29 @@
 package br.com.alura.java.io.teste;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 
 public class TesteLeitura {
 
 	public static void main(String[] args) throws IOException {
 
 		// Fluxo de entrada com arquivo
-		FileInputStream fis = new FileInputStream("lorem.txt");
-		InputStreamReader isr = new InputStreamReader(fis);
-		BufferedReader br = new BufferedReader(isr);
+		OutputStream fos = new FileOutputStream("lorem2.txt");
+		Writer osw = new OutputStreamWriter(fos);
+		BufferedWriter bw = new BufferedWriter(osw);
+
+		bw.write("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod");
+		bw.newLine();
+		bw.newLine();
+		bw.write("efioejfiwejfiewjfiwef");
 		
-		String linha = br.readLine();
 		
-		System.out.println(linha);
-		
-		br.close();
-		
+		bw.close();
+
 	}
 
 }
