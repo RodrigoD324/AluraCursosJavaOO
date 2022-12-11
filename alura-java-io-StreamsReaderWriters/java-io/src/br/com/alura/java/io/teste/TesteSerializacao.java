bd3eb4a213f0732f3f3c07783716fb271dc5ff81
RodrigoD324 <rodrigo.silvadeoliveira324@gmail.com>
2022-12-11 18:47:45 -0300

    realizando teste com serialização

--- alura-java-io-StreamsReaderWriters/java-io/src/br/com/alura/java/io/teste/TesteSerializacao.java
+++ alura-java-io-StreamsReaderWriters/java-io/src/br/com/alura/java/io/teste/TesteSerializacao.java
@@ -1,22 +1,25 @@
 package br.com.alura.java.io.teste;
 
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.io.ObjectInputStream;
 
 public class TesteSerializacao {
 
 	public static void main(String[] args) throws Exception {
-
-//		String nome = "Rodrigo Silva";
-//		ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("objeto.bin"));
-//		oos.writeObject(nome);
-//		oos.close();
 		
-		ObjectInputStream ois = new ObjectInputStream(new FileInputStream("objeto.bin"));
-		String nome = (String) ois.readObject();
+//		Cliente cliente = new Cliente();
+//		cliente.setNome("Rodrigo");
+//		cliente.setProfissao("Dev");
+//		cliente.setCpf("123456789");
+//
+//		ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("cliente.bin"));
+//		oos.writeObject(cliente);
+//		oos.close();
+//		
+		ObjectInputStream ois = new ObjectInputStream(new FileInputStream("cliente.bin"));
+		Cliente cliente = (Cliente) ois.readObject();
 		ois.close();
-		System.out.println(nome);
+		System.out.println(cliente.getCpf());
 		
 	}
 
---
+++ alura-java-io-StreamsReaderWriters/java-io/src/br/com/alura/java/io/teste/TesteSerializacaoCliente.java
@@ -0,0 +1,23 @@
+package br.com.alura.java.io.teste;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+public class TesteSerializacaoCliente {
+
+	public static void main(String[] args) throws Exception {
+
+//		String nome = "Rodrigo Silva";
+//		ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("objeto.bin"));
+//		oos.writeObject(nome);
+//		oos.close();
+		
+		ObjectInputStream ois = new ObjectInputStream(new FileInputStream("objeto.bin"));
+		String nome = (String) ois.readObject();
+		ois.close();
+		System.out.println(nome);
+		
+	}
+
+}
