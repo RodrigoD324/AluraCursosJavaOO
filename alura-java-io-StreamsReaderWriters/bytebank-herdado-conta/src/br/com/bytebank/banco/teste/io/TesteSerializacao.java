8de30e39fcc0ff0e29ebd167116761799dedcf7b
RodrigoD324 <rodrigo.silvadeoliveira324@gmail.com>
2022-12-11 19:05:54 -0300

    adicionei novo pacote 'br.com.bytebank.banco.teste.io'

--- alura-java-io-StreamsReaderWriters/bytebank-herdado-conta/src/br/com/bytebank/banco/teste/io/TesteSerializacao.java
+++ alura-java-io-StreamsReaderWriters/bytebank-herdado-conta/src/br/com/bytebank/banco/teste/io/TesteSerializacao.java
@@ -1,26 +1,26 @@
-package br.com.alura.java.io.teste;
+package br.com.bytebank.banco.teste.io;
 
 import java.io.FileOutputStream;
 import java.io.ObjectOutputStream;
 
+import br.com.bytebank.banco.modelo.Cliente;
+import br.com.bytebank.banco.modelo.ContaCorrente;
+
 public class TesteSerializacao {
 
 	public static void main(String[] args) throws Exception {
-		
 		Cliente cliente = new Cliente();
 		cliente.setNome("Rodrigo");
 		cliente.setProfissao("Dev");
 		cliente.setCpf("123456789");
 
+		ContaCorrente cc = new ContaCorrente(222, 333);
+		cc.setTitular(cliente);
+		cc.deposita(222.3);
+		
 		ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("cliente.bin"));
 		oos.writeObject(cliente);
 		oos.close();
-		
-//		ObjectInputStream ois = new ObjectInputStream(new FileInputStream("cliente.bin"));
-//		Cliente cliente = (Cliente) ois.readObject();
-//		ois.close();
-//		System.out.println(cliente.getCpf());
-		
 	}
 
 }
