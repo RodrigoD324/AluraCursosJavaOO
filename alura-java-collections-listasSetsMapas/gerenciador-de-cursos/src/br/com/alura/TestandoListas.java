3842dc1b3bbad6e921bcdcf81aaac8e2c677ea41
RodrigoD324 <rodrigo.silvadeoliveira324@gmail.com>
2022-12-12 18:35:18 -0300

    adicionei a classe 'TestandoListas' dentro do package 'br.com.alura'

---
+++ alura-java-collections-listasSetsMapas/gerenciador-de-cursos/src/br/com/alura/TestandoListas.java
@@ -0,0 +1,31 @@
+package br.com.alura;
+
+import java.util.ArrayList;
+
+public class TestandoListas {
+
+	public static void main(String[] args) {
+		String aula1 = "Conhecendo mais de listas";
+		String aula2 = "Modelando a classe Aula";
+		String aula3 = "Trabalhando com Cursos e Sets";
+		
+		ArrayList<String> aulas = new ArrayList<>();
+		aulas.add(aula1);
+		aulas.add(aula2);
+		aulas.add(aula3);
+		
+		System.out.println(aulas);
+		
+		aulas.remove(0);
+		
+		System.out.println(aulas);
+		
+		for (String aula : aulas) { // para cada string aula, dentro de aulas
+			System.out.println("Aula: " + aula);
+		}
+		
+		
+		
+	}
+
+}
