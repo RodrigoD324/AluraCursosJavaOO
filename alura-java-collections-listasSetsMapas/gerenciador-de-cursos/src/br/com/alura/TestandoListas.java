71502876cd7aa395af91f96a347aa161702f1c30
RodrigoD324 <rodrigo.silvadeoliveira324@gmail.com>
2022-12-12 20:57:42 -0300

    adicionei linhas a classe 'TestandoListas'

--- alura-java-collections-listasSetsMapas/gerenciador-de-cursos/src/br/com/alura/TestandoListas.java
+++ alura-java-collections-listasSetsMapas/gerenciador-de-cursos/src/br/com/alura/TestandoListas.java
@@ -8,24 +8,36 @@ public class TestandoListas {
 		String aula1 = "Conhecendo mais de listas";
 		String aula2 = "Modelando a classe Aula";
 		String aula3 = "Trabalhando com Cursos e Sets";
-		
+
 		ArrayList<String> aulas = new ArrayList<>();
 		aulas.add(aula1);
 		aulas.add(aula2);
 		aulas.add(aula3);
-		
+
 		System.out.println(aulas);
-		
+
 		aulas.remove(0);
-		
+
 		System.out.println(aulas);
-		
+
 		for (String aula : aulas) { // para cada string aula, dentro de aulas
 			System.out.println("Aula: " + aula);
 		}
-		
-		
-		
+
+		String primeiraAula = aulas.get(0);
+		System.out.println("A primeira aula e: " + primeiraAula);
+
+		for (int i = 0; i < aulas.size(); i++) {
+			System.out.println("Aula: " + aulas.get(i));
+		}
+
+		System.out.println(aulas.size());
+
+		aulas.forEach(aula -> {
+			System.out.println("Percorrendo: ");
+			System.out.println("Aula: " + aula);
+		});
+
 	}
 
 }
