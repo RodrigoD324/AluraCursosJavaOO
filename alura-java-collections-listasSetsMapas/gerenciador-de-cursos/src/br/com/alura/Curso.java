27b511114f79d4d54b43788e0c5f07919823e464
RodrigoD324 <rodrigo.silvadeoliveira324@gmail.com>
2022-12-13 18:50:37 -0300

    Modifiquei as classes 'Curso' e 'TestaCurso2'

--- alura-java-collections-listasSetsMapas/gerenciador-de-cursos/src/br/com/alura/Curso.java
+++ alura-java-collections-listasSetsMapas/gerenciador-de-cursos/src/br/com/alura/Curso.java
@@ -31,4 +31,12 @@ public class Curso {
 		this.aulas.add(aula);
 	}
 	
+	public int getTempoTotal() {
+		return this.aulas.stream().mapToInt(Aula::getTempo).sum();
+	}
+	
+	@Override
+	public String toString() {
+		return "[Curso: " + this.getNome() + ", tempo total: " + this.getTempoTotal() + ", aulas: " + this.getAulas() + "]";
+	}
 }
--- alura-java-collections-listasSetsMapas/gerenciador-de-cursos/src/br/com/alura/TestaCurso2.java
+++ alura-java-collections-listasSetsMapas/gerenciador-de-cursos/src/br/com/alura/TestaCurso2.java
@@ -1,5 +1,7 @@
 package br.com.alura;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class TestaCurso2 {
@@ -9,9 +11,18 @@ public class TestaCurso2 {
 		
 		javaColecoes.adiciona(new Aula("Trabalhando com ArrayLista", 21));
 		javaColecoes.adiciona(new Aula("Criando uma aula", 20));
-		javaColecoes.adiciona(new Aula("Modelando com colecoes", 22));
+		javaColecoes.adiciona(new Aula("Modelando com colecoes", 24));
 		
+		List<Aula> aulasImutaveis = javaColecoes.getAulas();
+		System.out.println(aulasImutaveis);
 		
+		List<Aula> aulas = new ArrayList<Aula>(aulasImutaveis);
+		
+		Collections.sort(aulas);
+		System.out.println(aulas);
+		System.out.println(javaColecoes.getTempoTotal());
+		
+		System.out.println(javaColecoes);
 		
 	}
 
