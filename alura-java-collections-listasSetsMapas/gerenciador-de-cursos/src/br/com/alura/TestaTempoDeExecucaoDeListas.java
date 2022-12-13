dd5aa967ef983ee35e183175a625ab1682455e75
RodrigoD324 <rodrigo.silvadeoliveira324@gmail.com>
2022-12-13 16:23:18 -0300

    Modifiquei o nome da classe para 'TestaTempoDeExecucaoDeListas'

--- alura-java-collections-listasSetsMapas/gerenciador-de-cursos/src/br/com/alura/TestaTempoDeExecucaoDeListas.java
+++ alura-java-collections-listasSetsMapas/gerenciador-de-cursos/src/br/com/alura/TestaTempoDeExecucaoDeListas.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
-public class TestaTempoDeExecucaoDeLista {
+public class TestaTempoDeExecucaoDeListas {
 
 	public static void main(String[] args) {
 
