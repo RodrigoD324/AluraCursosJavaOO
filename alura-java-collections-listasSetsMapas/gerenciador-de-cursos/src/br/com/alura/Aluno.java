1b93851c22ec63266600a8ad4fd2da3a1b0a67eb
RodrigoD324 <rodrigo.silvadeoliveira324@gmail.com>
2022-12-13 20:19:06 -0300

    Criei as classes 'Aluno' e 'TestaCursoComAluno', também modifiquei a classe 'Curso'

---
+++ alura-java-collections-listasSetsMapas/gerenciador-de-cursos/src/br/com/alura/Aluno.java
@@ -0,0 +1,26 @@
+package br.com.alura;
+
+public class Aluno {
+
+	private String nome;
+	private int numeroMatricula;
+
+	public Aluno(String nome, int numeroMatricula) {
+		this.nome = nome;
+		this.numeroMatricula = numeroMatricula;
+	}
+
+	public String getNome() {
+		return this.nome;
+	}
+
+	public int getNumeroMatricula() {
+		return this.numeroMatricula;
+	}
+
+	@Override
+	public String toString() {
+		return "[Aluno: " + this.nome + ", matricula: " + this.numeroMatricula + "]";
+	}
+
+}
--- alura-java-collections-listasSetsMapas/gerenciador-de-cursos/src/br/com/alura/Curso.java
+++ alura-java-collections-listasSetsMapas/gerenciador-de-cursos/src/br/com/alura/Curso.java
@@ -1,6 +1,8 @@
 package br.com.alura;
 
+import java.util.Collection;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -9,6 +11,7 @@ public class Curso {
 	private String nome;
 	private String instrutor;
 	private List<Aula> aulas = new LinkedList<Aula>();
+	private Collection<Aluno> alunos = new HashSet<>();
 	
 	public Curso(String nome, String instrutor) {
 		this.nome = nome;
@@ -38,5 +41,13 @@ public class Curso {
 	@Override
 	public String toString() {
 		return "[Curso: " + this.getNome() + ", tempo total: " + this.getTempoTotal() + ", aulas: " + this.getAulas() + "]";
+	}
+
+	public void matricula(Aluno aluno) {
+		this.alunos.add(aluno);
+	}
+	
+	public Collection<Aluno> getAlunos() {
+		return Collections.unmodifiableCollection(alunos);
 	}
 }
